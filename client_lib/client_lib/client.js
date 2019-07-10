a9c2fc14f460c68a33bfd773ab34fd8d8f8af9b2
Vimal Sheoran <vimalpsheoran@gmail.com>
2019-07-10 08:18:20 +0530

    Added client library.

---
+++ client_lib/client_lib/client.js
@@ -0,0 +1,46 @@
+const axios = require("axios");
+const uuid = require("uuid/v4");
+const socket = require("socket.io-client");
+
+function Client(server) {
+
+	this.server = server;
+	Client.clientId = uuid();
+	Client.socket = socket(server);
+
+	this.channels = {
+		"feedback": `${this.socketId}_feedback`
+	}
+
+	this.sendData = async (endpoint, payload) => {
+		try {
+			let response = await axios.post(
+				`${this.server}${endpoint}`, 
+				{"data": payload, "clientId": Client.clientId}
+			);
+			return response.data;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	this.listenOnSocket = async () => {
+		Client.socket.on("httpFeedback", (msg) => console.log(msg));
+		Client.socket.on("success", msg => console.log("Successfully registered packet: ", msg));
+		Client.socket.on("failure", msg => console.log("Failed to register packet: "+msg+". Please consider retrying."));
+	}
+}
+
+function InitClient() {
+	return new Promise((res, rej) => {
+		Client.socket.emit("init", Client.clientId);
+		setTimeout(() => {
+			res();
+		}, 2000);
+	})
+}
+
+module.exports = {
+	"Client": Client,
+	"InitClient": InitClient
+}
---
+++ client_lib/client_lib/sample_implementation.js
@@ -0,0 +1,53 @@
+const Client = require("./client").Client;
+const InitClient = require("./client").InitClient;
+const SERVER_URL = "http://localhost:5000";
+const fs = require("fs");
+let payload = [
+	{
+		"id": "1",
+		"user_id": "2",
+		"vehicle_model_id": "3",
+		"package_id": "4",
+		"travel_type_id": "5",
+		"from_area_id": "6",
+		"to_area_id": "7",
+		"from_city_id": "8",
+		"to_city_id": "9",
+		"from_date": "10",
+		"to_date": "11",
+		"online_booking": "12",
+		"mobile_site_booking": "13",
+		"booking_created": "14",
+		"from_lat": "15",
+		"from_long": "16",
+		"to_lat": "17",
+		"to_long": "18",
+		"car_cancellation": "19"	
+	}
+]
+
+let rawData = fs.readFileSync("data.json");
+let jsonData = JSON.parse(rawData);
+let i = 0;
+const client = new Client(SERVER_URL);
+
+const sendData = () => {
+	setInterval( async () => {
+		let offset = 10;
+		if (i > 150) clearInterval(sendData);
+		let dataToSend = jsonData.slice(i, i+offset);
+		i += offset;
+		let resp = await client.sendData("/data", dataToSend);
+		console.log(resp);
+	}, 30000);
+}
+
+InitClient().then( async () => {
+	sendData();
+	// setInterval( async () => {
+	// 	let resp = await client.sendData("/data", jsonData);
+	// 	console.log(resp);
+	// }, 30000)
+	client.listenOnSocket();
+})
+.catch(err => console.log(err));
