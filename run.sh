0b81e24c90712c7ac8bb9df4ad594224e09a28a9
Vimal Sheoran <vimalpsheoran@gmail.com>
2019-07-10 12:03:13 +0530

    Fixed bug.

--- run.sh
+++ run.sh
@@ -3,6 +3,4 @@ echo "==============================================="
 echo "============== ALL SYSTEMS GO ================="
 echo "==============================================="
 
-gonme-terminal -e "node appserver/app.js"
-gnome-terminal -e "node queue_scraper/app.js"
-gnome-terminal -e "node client_lib/client_lib/sample_implementation.js"
+gnome-terminal -e "node appserver/app.js" && gnome-terminal -e "node queue_scraper/app.js" && gnome-terminal -e "node client_lib/client_lib/sample_implementation.js"
